16c46f5c030fb0777c762aa46feddfad9bb59560
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-08-14 22:10:16 +0530

    Update BuyAndSellStocks.java

--- BuyAndSellStocks.java
+++ BuyAndSellStocks.java
@@ -1,4 +1,4 @@
-                                //BUY AND SELL STOCKS 
+                              //BUY AND SELL STOCKS 
 /*You are given an array prices where prices[i] is the price of a given stock on the ith day. 
 You want to maximize your profit by choosing a single day to buy one stock and choosing a 
 different day in the future to sell that stock. Return the maximum profit you can achieve 
