6682601b81e3ea1383a745849b05750c451a01ca
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-08-19 17:58:02 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,34 @@
+package JAVA;
+
+public class SelectionSort {
+    public static void Sort(int arr[]){ //Time complexcity is big O of n^2 
+        for(int i= 0; i<arr.length-1;i++){
+            int minPos = i ;
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[minPos]>arr[j]){ // Incerasing 
+                    minPos = j;
+                } 
+            }
+            //Swaping
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    public static void PrintSelectionSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        Sort(arr);
+        PrintSelectionSort(arr);
+
+        
+    }
+    
+}
