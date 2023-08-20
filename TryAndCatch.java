9e790a20a99549f47af027131243dbdd28a9139c
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-08-20 17:39:32 +0530

    Add files via upload

---
+++ TryAndCatch.java
@@ -0,0 +1,26 @@
+package JAVA;
+
+public class TryAndCatch {
+    public static void main(String[] args){
+        int a = 1000;
+        int b = 0;
+
+        //Without Try:
+        // int c = a/b;
+        // System.out.println("The result is "+c);
+
+        //With Try
+        try{
+            int c =a/b;
+            System.out.println("The result is "+c);
+        }
+        catch (Exception e) {//If the exception occurs, it will be caught here and handled by this block of code
+            System.out.println("We faild to devide.");
+            System.out.print("Reason:- ");
+            System.out.println(e);
+        }
+        System.out.println("The End");
+    }
+    
+    
+}
