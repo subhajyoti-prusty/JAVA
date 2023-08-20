4c3cecefe75c20531a3781e3e1c78633c3d0ebc4
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-08-20 17:40:09 +0530

    Add files via upload

---
+++ PairsInArray.java
@@ -0,0 +1,24 @@
+package JAVA;
+public class PairsInArray {
+
+    public static void PrintPair(int number[]){
+        int tp = 0;
+        for(int i=0;i<number.length;i++){
+            int current = number[i];
+            for(int j=i+1;j<number.length;j++){
+                System.out.print("("+current+";"+number[j]+")");
+                tp++;
+            }
+            System.out.println();
+            
+        }
+        
+        System.out.println("Total Pairs ="+tp);
+    }
+    public static void main (String args[]){
+        int number[] = {1,2,3,4,5,6,7,8,9};
+        PrintPair(number);
+
+    }
+    
+}
