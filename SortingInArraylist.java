9f0226152651adf21cf74430ac55be229cd8924e
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-09-07 21:14:48 +0530

    Add files via upload

---
+++ SortingInArraylist.java
@@ -0,0 +1,22 @@
+package ArrayList.src;
+import java.util.ArrayList;
+import java.util.Collections;
+public class SortingInArraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer>list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        System.out.println(list);
+
+        //Ascending order
+        Collections.sort(list);
+        System.out.println(list);
+
+        //Descending order
+        Collections.sort(list, Collections.reverseOrder(null));
+        System.out.println(list);
+    }
+}
