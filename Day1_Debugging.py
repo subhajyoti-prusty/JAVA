2bffd319dff3547e011fc92365d595fdeff07f55
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-10-27 23:03:47 +0530

    Add files via upload

---
+++ Day1_Debugging.py
@@ -0,0 +1,17 @@
+# Look at the code in the code editor on the left. 
+# There are errors on all 4 lines of code. 
+# Fix the code so that it runs without errors.
+
+# Fix the code below 👇
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
+
+#Code after debugging
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " '"+"' " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
