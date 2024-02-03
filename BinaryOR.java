c34a24cb6e3d960ebde98bef8b9458323f4659fe
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2024-02-04 01:26:10 +0530

    Add files via upload

---
+++ BinaryOR.java
@@ -0,0 +1,11 @@
+package BitManipulation.src;
+
+public class BinaryOR {
+    public static void main(String[] args) {
+        System.out.println(5 | 6);
+        System.out.println(4 | 6);
+        System.out.println(5 | 3);
+        System.out.println(7 | 1);
+        System.out.println(0 | 8);
+    }
+}
