328a0bdef8a58e6699e94d1dde9cead8827222ca
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2024-02-05 12:21:50 +0530

    Add files via upload

---
+++ BinaryNot.java
@@ -0,0 +1,11 @@
+package BitManipulation.src;
+
+public class BinaryNot {
+    public static void main(String[] args) {
+        System.out.println( ~ 5);
+        System.out.println( ~ 6);
+        System.out.println( ~ 3);
+        System.out.println( ~ 1);
+        System.out.println( ~ 8);
+    }
+}
