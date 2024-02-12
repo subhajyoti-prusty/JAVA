4b0c43d9a6fd40b8189e31b049407eebac78fa54
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2024-02-13 00:17:03 +0530

    Add files via upload

---
+++ IntervalSheduling.java
@@ -0,0 +1,47 @@
+package GreedyAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class IntervalSheduling {
+    public static void main(String[] args) {
+        int start[] = { 1, 3, 0, 5, 8, 5 };
+        int end[] = { 2, 4, 6, 7, 9, 9 };
+
+        // sorting
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = 1;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        // lambda function -> shortform I
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
+        // end time basis sorted
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        // 1st activity
+        maxAct = 1;
+        ans.add(0);
+        int lastEnd = end[0];
+        for (int i = 1; i < end.length; i++) {
+            if (start[i] >= lastEnd) {
+                // activity select
+                maxAct++;
+                ans.add(i);
+                lastEnd = end[i];
+            }
+        }
+
+        System.out.println("max activities = " + maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+
+}
